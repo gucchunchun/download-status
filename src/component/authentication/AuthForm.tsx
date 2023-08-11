db13fd44a4049448518ad7fb26c38d32b4c0c83a
gucchunchun <yunataniguchi23@gmail.com>
2023-08-11 03:24:26 +0100

    add func handleSignUpSubmit

--- src/component/authentication/AuthForm.tsx
+++ src/component/authentication/AuthForm.tsx
@@ -5,7 +5,7 @@ interface AuthFormProps  {
     handleLogin:(index:number, user:Type.User) => void;
 }
 const AuthForm:React.FC<AuthFormProps> = (props) => {
-    const [isLogin, setIsLogin] = useState<boolean>(true);
+    const [isLogin, setIsLogin] = useState<boolean>(false);
     const [id, setId] = useState("");
     const [pwd, setPwd] = useState("");
     const [error, setError] = useState(null);
@@ -39,6 +39,29 @@ const AuthForm:React.FC<AuthFormProps> = (props) => {
     }
     function handleSignUpSubmit(e:React.FormEvent<HTMLFormElement>):void {
         e.preventDefault();
+        fetch('/api/signUp', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id, pwd })
+        })
+        .then((res)=> {
+            if(res.status===401) {
+                return res.json().then((errData)=>{
+                    throw new Error(errData);
+                })
+            }
+            return res.json()
+        })
+        .then((data:{message:string, token:number, user:Type.User})=>{
+            alert(data.message);
+            props.handleLogin(data.token, data.user);
+        })
+        .catch((err)=>{
+            setError(err.message);
+            console.log(err.message);
+        });
     }
     return(
         <form onSubmit={isLogin? handleLoginSubmit: handleSignUpSubmit}>
