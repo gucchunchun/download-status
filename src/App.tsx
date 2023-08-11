c7a950739f2fd9a00a64f9e2502b308558a10f45
gucchunchun <yunataniguchi23@gmail.com>
2023-08-11 01:48:10 +0100

    change to Arrow func component
    I am going to use ...
    *Arrow function for components
    *'[state, setState]' for useState (I was using both 'setState' and 'updateState' )

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
 import React, {useState, useEffect} from 'react';
 import * as Type from './Type';
 
-export default function App() {
-    const [menu, updateMenu] = useState<Type.File[]>(FilesMenu);
-    const [files, updateFiles] = useState<Type.File[]>(test);
-    const [used, updateUsed] = useState<number>(0);
-    const [willbeused, updateWillbeUsed] = useState<number>(0);
+const App:React.FC = () => {
+    const [userData, setUserData] = useState<(Type.User|null)>(null);
+    const [files, setFiles] = useState<(Type.File[]|null)>(null);
+    const [menu, setMenu] = useState<(Type.File[]|null)>(null);
+    const [used, setUsed] = useState<number>(0);
+    const [willUsed, setWillUsed] = useState<number>(0);
   
     return (
       <>
       </>
     );
-  } 
+} 
+export default App;
 
 //   useEffect(() => {
 //     const intervalId = setInterval(() => {
 //       // Code to be executed on each interval tick
-//       updateFiles((prevFiles) => {
-//         const updatedFiles = prevFiles.map((file) => {
-//           let updatedStatus = file.status;
+//       setFiles((prevFiles) => {
+//         const setdFiles = prevFiles.map((file) => {
+//           let setdStatus = file.status;
 //           if (typeof file.status === "string") {
 //               if(file.status === "waiting" && Math.random() < 0.1){
-//                   updatedStatus = 0;
+//                   setdStatus = 0;
 //               }
 //           }else if  (Math.random() < 0.2){
-//               updatedStatus = file.status + 1;
+//               setdStatus = file.status + 1;
 //           }else if (Math.random() < 0.5){
-//               updatedStatus = file.status + 2;
+//               setdStatus = file.status + 2;
 //           }
 //           return {
 //             ...file,
-//             status: (typeof updatedStatus=="number"&&updatedStatus > 100) ? "up to date" : updatedStatus,
+//             status: (typeof setdStatus=="number"&&setdStatus > 100) ? "up to date" : setdStatus,
 //           };
 //         });
-//         return updatedFiles;
+//         return setdFiles;
 //       });
 //     }, 100); // Interval time in milliseconds (1000ms = 1 second)
