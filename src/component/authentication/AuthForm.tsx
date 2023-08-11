e819fdbae51fd6f11e6674bf63f9e8b7d9b17e21
gucchunchun <yunataniguchi23@gmail.com>
2023-08-11 02:09:50 +0100

    move legacies to Waiting_refactor in parents folder

---
+++ src/component/authentication/AuthForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as Type from '../../Type';
+
+interface AuthFormProps  {
+
+}
+const AuthForm:React.FC<AuthFormProps> = (props) => {
+
+    return(
+        <>
+            <input></input>
+        </>
+    )
+}
+
+export default AuthForm;
