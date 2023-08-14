ccbb036105576d3806ae763affc856efb02f3443
gucchunchun <yunataniguchi23@gmail.com>
2023-08-14 17:50:17 +0100

    add global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -8,21 +8,27 @@ const globalStyles = css`
         background-color: rgb(${theme.colors.bg});
         margin: 0;
         padding: 0;
+        color: rgb(${theme.colors.textPrimary}));
     }
     h1 {
         font-size: 3.052rem;
+        font-weight: bold;
     }
     h2 {
         font-size: 2.441rem;
+        font-weight: bold;
     }
     h3 {
         font-size: 1.953rem;
+        font-weight: bold;
     }
     h4 {
         font-size: 1.563rem;
+        font-weight: bold;
     }
     h5 {
         font-size: 1.25rem;
+        font-weight: bold;
     }
     p {
         font-size: 1rem;
