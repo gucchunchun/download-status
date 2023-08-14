5baa5198eb271dae8f4872b345cf2b7cae96f573
gucchunchun <yunataniguchi23@gmail.com>
2023-08-14 22:36:05 +0100

    add TextButton Component

---
+++ src/component/common/buttons/TextButton.tsx
@@ -0,0 +1,46 @@
+import React, { MouseEventHandler} from 'react';
+import styled from '@emotion/styled';
+
+interface TextButtonProps {
+    width?: string
+    height?: string
+    type?: "button" | "submit" | "reset"
+    text: string
+    textColor?: string
+    hoveredTextColor?: string
+    onClick: MouseEventHandler
+}
+interface StyledButtonProps {
+    text: string
+    width?: string
+    height?: string
+    color?: string
+    hoveredColor?: string
+}
+
+const StyledButton = styled('button')<StyledButtonProps>`
+    width: ${props=>props.width || props.text.length + 'rem'}
+    height: ${props=>props.height || 'fit-content'}
+    color: ${props=>props.color || '#DCDCDC'}
+    &:hover {
+        color: ${props=>props.hoveredColor || '#000'}
+    }
+`;
+
+const TextButton:React.FC<TextButtonProps> = (props) => {
+    return(
+        <>
+            <StyledButton 
+                text={props.text}
+                width={props.width}
+                height={props.height}
+                color={props.textColor}
+                hoveredColor={props.hoveredTextColor}
+                type={props.type}
+                onClick={props.onClick}
+            />
+        </>
+    )
+}
+
+export default TextButton;
